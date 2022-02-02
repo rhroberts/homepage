e9e7bba5d5dd717dc3c2b3c6e6db16a6049c2b9d
Rusty Roberts <rust.roberts@protonmail.com>
2022-02-01 22:42:01 -0800

    Improve handler, add favicon

--- app.tsx
+++ app.tsx
@@ -1,28 +1,62 @@
 /** @jsx h */
 
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
-import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+import {
+  h,
+  Helmet,
+  renderSSR,
+} from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
 
 function App() {
   return (
-    <html>
-      <head>
-        <title>Hello from JSX and Deno!</title>
-      </head>
-      <body>
-        <h1>Hello, world!</h1>
-      </body>
-    </html>
+    <div>
+      <Helmet>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>rhroberts.dev</title>
+      </Helmet>
+      <h1>Yes. Welcome.</h1>
+    </div>
   );
 }
 
-function handler(req: Request) {
-  const html = renderSSR(<App />);
-  return new Response(html, {
-    headers: {
-      "content-type": "text/html",
-    },
-  });
+const ssr = renderSSR(<App />);
+const { body, head, footer } = Helmet.SSR(ssr);
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    ${head.join("\n")}
+  </head>
+  <body>
+    ${body}
+    ${footer.join("\n")}
+  </body>
+</html>
+`;
+
+async function handler(req: Request) {
+  const { pathname } = new URL(req.url);
+  console.log(pathname);
+  switch (pathname) {
+    case "/favicon.ico": {
+      const favicon = await Deno.readFile("./static/favicon.ico");
+      return new Response(favicon, {
+        headers: {
+          "content-type": "image/x-icon",
+        },
+      });
+    }
+    case "/":
+      return new Response(html, {
+        headers: {
+          "content-type": "text/html",
+        },
+      });
+    default:
+      return new Response("Not found.", { status: 404 });
+  }
 }
 
 console.log("Listening on http://localhost:8000");
