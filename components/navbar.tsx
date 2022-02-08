c0ec175bd952e6f2c970ed498e7ade967e238c17
Rusty Roberts <rust.roberts@protonmail.com>
2022-02-07 23:18:13 -0800

    break navbar out into component

---
+++ components/navbar.tsx
@@ -0,0 +1,38 @@
+/** @jsx h */
+
+import { h } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+
+export default function NavBar(props: { activePage: string }) {
+  const { activePage } = props;
+  return (
+    <div id="navbar">
+      <div>
+        {"["}rhroberts.dev{"]&nbsp;&nbsp;"}
+        <NavItem
+          name="Home"
+          href="/"
+          current={activePage === "Home"}
+        />
+        {" &bull; "}
+        <NavItem
+          name="Projects"
+          href="/projects"
+          current={activePage === "Projects"}
+        />
+        {" &bull; "}
+        <NavItem
+          name="Resume"
+          href="/resume"
+          current={activePage === "Projects"}
+        />
+      </div>
+    </div>
+  );
+}
+
+function NavItem(props: { name: string; href: string; current: boolean }) {
+  const { name, href, current } = props;
+  return (
+    <a href={href} class={current ? "nav-active" : "nav-inactive"}>{name}</a>
+  );
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 
 import { h, Helmet } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
+import NavBar from "../components/navbar.tsx";
 
 export default function Home() {
   return (
@@ -8,23 +9,26 @@ export default function Home() {
       <Helmet>
         <title>Home</title>
       </Helmet>
+      <NavBar activePage="Home" />
       <div class="content">
-        <h1>Welcome.</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci nulla
-          pellentesque dignissim enim sit amet venenatis urna cursus. Viverra
-          tellus in hac habitasse. Interdum varius sit amet mattis vulputate.
-          Tellus in metus vulputate eu scelerisque felis imperdiet proin
-          fermentum. Lacus sed turpis tincidunt id aliquet risus feugiat in.
-          Mauris in aliquam sem fringilla ut morbi tincidunt augue interdum.
-          Volutpat diam ut venenatis tellus in metus. Leo a diam sollicitudin
-          tempor id eu nisl nunc. Bibendum est ultricies integer quis auctor
-          elit sed vulputate. Non curabitur gravida arcu ac tortor dignissim.
-          Lacinia quis vel eros donec. Ut placerat orci nulla pellentesque
-          dignissim enim sit. Nulla posuere sollicitudin aliquam ultrices.
-          Placerat orci nulla pellentesque dignissim enim sit..
-        </p>
+        <div id="main">
+          <h1>Welcome.</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
+            nulla pellentesque dignissim enim sit amet venenatis urna cursus.
+            Viverra tellus in hac habitasse. Interdum varius sit amet mattis
+            vulputate. Tellus in metus vulputate eu scelerisque felis imperdiet
+            proin fermentum. Lacus sed turpis tincidunt id aliquet risus feugiat
+            in. Mauris in aliquam sem fringilla ut morbi tincidunt augue
+            interdum. Volutpat diam ut venenatis tellus in metus. Leo a diam
+            sollicitudin tempor id eu nisl nunc. Bibendum est ultricies integer
+            quis auctor elit sed vulputate. Non curabitur gravida arcu ac tortor
+            dignissim. Lacinia quis vel eros donec. Ut placerat orci nulla
+            pellentesque dignissim enim sit. Nulla posuere sollicitudin aliquam
+            ultrices. Placerat orci nulla pellentesque dignissim enim sit..
+          </p>
+        </div>
       </div>
     </div>
   );
