83a4083fdb3e1ea4537962ad623f5b0ae7db82ce
Rusty Roberts <rust.roberts@protonmail.com>
2022-05-09 20:54:11 -0700

    start on resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -15,7 +15,31 @@ export default function Resume() {
       </Helmet>
       <NavBar activePage="Resume" />
       <div id="content">
-        <div id="main"></div>
+        <div id="main">
+          <div id="resume">
+            <h1>Richard (Rusty) H. Roberts</h1>
+            <address>
+              <a href="mailto:mail@rhroberts.dev">mail @ rhroberts.dev</a>{" "}
+              &bullet;
+              <a
+                href="https://github.com/rhroberts"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github.com/rhroberts
+              </a>
+              &bullet;
+              <a
+                href="https://git.sr.ht/~rustyr/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                git.sr.ht/~rustyr
+              </a>
+            </address>
+            <em>Currently based in Seattle, WA.</em>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
