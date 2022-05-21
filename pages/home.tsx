164907a37e968653b0344c0b509f281265305306
Rusty Roberts <rust.roberts@protonmail.com>
2022-05-21 16:28:38 -0700

    update about me section

--- pages/home.tsx
+++ pages/home.tsx
@@ -20,19 +20,44 @@ export default function Home() {
         <div id="main">
           <h1>Welcome.</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
-            nulla pellentesque dignissim enim sit amet venenatis urna cursus.
-            Viverra tellus in hac habitasse. Interdum varius sit amet mattis
-            vulputate. Tellus in metus vulputate eu scelerisque felis imperdiet
-            proin fermentum. Lacus sed turpis tincidunt id aliquet risus feugiat
-            in. Mauris in aliquam sem fringilla ut morbi tincidunt augue
-            interdum. Volutpat diam ut venenatis tellus in metus. Leo a diam
-            sollicitudin tempor id eu nisl nunc. Bibendum est ultricies integer
-            quis auctor elit sed vulputate. Non curabitur gravida arcu ac tortor
-            dignissim. Lacinia quis vel eros donec. Ut placerat orci nulla
-            pellentesque dignissim enim sit. Nulla posuere sollicitudin aliquam
-            ultrices. Placerat orci nulla pellentesque dignissim enim sit..
+            My name is Rusty Roberts. I'm a materials scientist turned software
+            engineer based in Seattle, Washington. I write web and desktop
+            applications. I am an ardent open-source and free software
+            enthusiast, and by extension a fan of the Linux ecosystem.
+          </p>
+          <p>
+            Professionally, I develop full-stack web applications for the{" "}
+            <a href="https://www.twdb.texas.gov" target="_blank">
+              Texas Water Development Board
+            </a>{" "}
+            and maintain the servers they run on. For backend work, my language
+            of choice is often Python and I have experience in frameworks like
+            Flask and FastAPI. For frontend work, I have experience writing
+            modern React applications in TypeScript. I enjoy learning new
+            languages and technologies, and am always open to using the best
+            tool for the task at hand.
+          </p>
+          <p>
+            Unprofessionally, I enjoy making things that combine music and code,
+            like{" "}
+            <a href="https://rhroberts.github.io/browsyn/" target="_blank">
+              this
+            </a>{" "}
+            analog-imitating software synthesizer built on the Web Audio API. Or
+            producing music and contributing audio-related code for{" "}
+            <a href="https://github.com/dmarc3/bakken" target="_blank">
+              hobby video games
+            </a>.
+          </p>
+          <p>
+            From here, you can read about a few select{" "}
+            <a href="/projects">projects</a> of mine, view my{" "}
+            <a href="/resume">resume</a>, our checkout my public repositories on
+            {" "}
+            <a href="https://github.com/rhroberts" target="_blank">GitHub</a>.
+          </p>
+          <p>
+            Thanks for stopping by.
           </p>
           <Contact />
         </div>
