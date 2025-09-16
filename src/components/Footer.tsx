fa23c5991e8ecbefa5d5bdfe695c8c2905afea54
Rusty Roberts <rust.roberts@protonmail.com>
2025-09-16 08:59:25 -0700

    feat: contact info in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,35 @@ export default function Footer() {
     <div className={styles.footerWrapper}>
       <footer className={styles.footer}>
         <div className={styles.footerContent}>
-          <span className={styles.footerText}>© 2025 • Rusty Roberts</span>
+          <span className={styles.footerText}>ⓒ 2025 • Rusty Roberts</span>
+          <span className={styles.footerText}>•</span>
+          <div className={styles.contactIcons}>
+            <a href="mailto:mail@rhroberts.dev" aria-label="Email">
+              <svg className={`${styles.contactIcon} icon-mail`}>
+                <use xlinkHref="#icon-mail"></use>
+              </svg>
+            </a>
+            <a
+              href="https://matrix.to/#/@rustyr:matrix.org"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Matrix"
+            >
+              <svg className={`${styles.contactIcon} icon-bubble`}>
+                <use xlinkHref="#icon-bubble"></use>
+              </svg>
+            </a>
+            <a
+              href="https://github.com/rhroberts"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub"
+            >
+              <svg className={`${styles.contactIcon} icon-git`}>
+                <use xlinkHref="#icon-git"></use>
+              </svg>
+            </a>
+          </div>
         </div>
         <div className={styles.footerRight}>
           <a
